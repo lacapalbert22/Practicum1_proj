975e8ac1efd8ae6b4e2746d172dfb6d370d743b7
lacapalbert22 <lacapalbert22@gmail.com>
2018-03-16 01:59:05 +0800

    fix stuff

--- Practicum 1-Project/css/addsched.js
+++ Practicum 1-Project/css/addsched.js
@@ -9,11 +9,32 @@ btn.onclick = function() {
 span.onclick = function() {
     modal.style.display = "none";
 }
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+
+var modal1 = document.getElementById('view-par');
+var btn1 = document.getElementById("viewpar");
+var span1= document.getElementsByClassName("close1")[0];
+
+btn1.onclick = function() {
+   modal1.style.display = "block";
 }
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+
+var modal2 = document.getElementById('del-sched');
+var btn2 = document.getElementById("delsched");
+var span2 = document.getElementsByClassName("close2")[0];
+
+btn2.onclick = function() {
+   modal2.style.display = "block";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+
+
 
 
 
