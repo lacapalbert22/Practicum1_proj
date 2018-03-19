bd76881e3f242334d7f3ac64cc0f6e435e9a061e
lacapalbert22 <lacapalbert22@gmail.com>
2018-03-20 00:29:58 +0800

    add files again

---
+++ Practicum 1-Project/schedule.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<?php
+ include_once 'dbh.inc.php';
+?>
+<html lang="en">
+    <head>        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Schedule</title>
+        <link rel="icon" href="images/slulogo.png">
+       
+        
+        <link rel="stylesheet" type="text/css" href="css/style.css" title="main">
+    </head>
+    <body class ="color">
+<?php
+	$sql = "SELECT * FROM mytable";
+	$result = mysqli_query($connection, $sql);
+	$resultCheck = mysqli_num_rows($result);
+	
+	if ($resultCheck > 0) {
+		while ($row = mysqli_fetch_assoc($result)) {
+		   echo $row['id'] . "<br>";
+		}
+	}
+?>	
+
+        <div class="boxhead">
+        <div class ="header-container">
+         <img src="images/slulogo.png" class="slulogo">
+         <input type="submit" name="" value="Log-out">
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="home.html">HOME</a></li>
+                <li><a href="company.html">COMPANY</a></li>
+                <li class="selected"><a href="schedule.html">SCHEDULE</a></li>
+            </ul>
+        </nav>
+<button id="addsched">Add Schedule</button>
+<div id="add-sched" class="modal">
+    <div class="modal-content">
+    <span class="close">&times;</span>
+        <table class="form1">
+        <tr>
+            <td>Event Name</td>
+            <td><input type="text" name="Event_name" value=""></td>
+        </tr>
+        <tr>
+            <td>Event Description</td>
+            <td><input type="text" name="Event_Description" value=""></td>
+        </tr>
+        <tr>
+            <td>Venue</td>
+            <td><input type="text" name="Venue" value=""></td>
+        </tr>
+        <tr>
+            <td>Class</td>
+            <td><input type="text" name="roll_no" value=""></td>
+            </td>
+        </tr>
+        <tr>
+        <td>Gender</td>
+        <td>
+        <select name="gender">
+            <option value="">Please Select</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+        </td>
+    </tr>
+    <tr>
+        <input class="save" type="submit" value="save">
+        <input class="cancel" type="submit" value="cancel">
+    </tr>
+    </table>
+
+    </div>
+</div>
+
+<button id="viewpar">View Participants</button>
+<div id="view-par" class="modal1">
+    <div class="modal-content">
+    <span class="close1">&times;</span>
+    <p>For View Participants</p>
+    </div>
+</div>
+
+
+<button id="delsched">Delete Schedule</button>
+<div id="del-sched" class="modal2">
+    <div class="modal-content">
+    <span class="close2">&times;</span>
+
+    </div>
+</div>
+
+
+
+<script src="css/addsched.js"></script>
+
+		 
+    </body>
+
+    <footer>
+        <p>Copyright &copy; Saint Louis University 2018 All Rights Reserved.</p>
+    </footer>
+</html>
